22aca5e7dd7b5d176ce30df19fd5312c4b7a1c78
sebdev <sebdeveloper6952@gmail.com>
2024-01-29 16:01:05 +0000

    initial commit

---
+++ src/lib/components/buttons/OutlinedButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let expanded = false;
+	export let disabled = false;
+</script>
+
+<button
+	on:click
+	{disabled}
+	class:w-full={expanded}
+	class:w-fit={!expanded}
+	class="px-6 py-1 rounded-lg font-bold text-sm text-orange-700 border border-orange-700 hover:bg-orange-600/10 active:bg-orange-700/10 disabled:cursor-not-allowed disabled:hover:bg-orange-700/10"
+>
+	<slot />
+</button>
---
+++ src/lib/components/buttons/TextButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let expanded = false;
+	export let disabled = false;
+</script>
+
+<button
+	on:click
+	{disabled}
+	class:w-full={expanded}
+	class:w-fit={!expanded}
+	class="px-6 py-1 rounded-lg text-orange-700 font-bold hover:bg-orange-600/10 active:bg-orange-700/10 disabled:cursor-not-allowed disabled:hover:bg-orange-700/10"
+>
+	<slot />
+</button>
